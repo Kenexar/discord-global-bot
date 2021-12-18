32cd231cd8f24d325fc3c8b45529443ac21f09b5
exersalza <squidodle44@gmail.com>
2021-12-19 00:08:09 +0100

    augenkrebs

--- KenexarGlobalBot/Main.cs
+++ KenexarGlobalBot/Main.cs
@@ -1,9 +1,41 @@
-﻿namespace KenexarGlobalBot;
+﻿using Discord;
+using Discord.Addons.Hosting;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-public class Start
+namespace KenexarGlobalBot
 {
-    public static void Main(string[] args)
+    public class Start
     {
-        Console.WriteLine("coggers");
+        static async Task Main()
+        {
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration(x =>
+                {
+                    var config = new ConfigurationBuilder() // create config
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("config.json", false, true)
+                        .Build();
+                    
+                    x.AddConfiguration(config);
+                })
+                
+                .ConfigureLogging(x =>
+                {
+                    x.AddConsole();
+                    x.SetMinimumLevel(LogLevel.Debug);
+                })
+                
+                .ConfigureDiscordHost<DiscordSocketClient>((ctx, config) =>
+                {
+                    config.SocketConfig = new DiscordSocketConfig
+                    {
+                        LogLevel = Discord.LogServerity.Debug,
+                        AlwaysDownloadUsers = "" // ich krieg hier noch augenkrebs holy shit
+                    }
+                })
+        }
     }
 }
